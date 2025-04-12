055b2ae991d439d48ba257f1c37ff0ae42604dc6
RaZe⚜️ <official.razeglobal@gmail.com>
2025-04-12 17:11:14 +0530

    Create _app.tsx

---
+++ pages/_app.tsx
@@ -0,0 +1,7 @@
+// pages/_app.tsx
+import '@/styles/globals.css
+import type f AppProps > from 'next/app
+function MyApp(f Component, pageProps 7:
+AppProps) f
+return <Component f...pagePropst />
+export default MyApp
