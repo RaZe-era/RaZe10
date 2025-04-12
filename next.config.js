5907ffd83dbda3ebac41ca081bca553859e596a4
RaZe⚜️ <official.razeglobal@gmail.com>
2025-04-12 16:21:06 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // You might need to add image domains here if using external images
+  // images: {
+  //   domains: ['example.com'],
+  // },
+}
+
+module.exports = nextConfig
